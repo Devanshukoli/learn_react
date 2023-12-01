ba36d84e95ac8997af041b8e2c1fb17d58814507
Devanshukoli <kolidevanshu02@gmail.com>
2023-12-01 19:35:00 +0530

    Ready for build.

--- 20DaysOfReact/day1/vite-project/src/component/Welcome.jsx
+++ 20DaysOfReact/day1/vite-project/src/component/Welcome.jsx
@@ -1,9 +1,13 @@
+import Sammy from '../img/small.jpeg'
+import '../css/main.css'
+
 export default function Welcome() {
 	return (
 		<>
 			<div className='wrapper'>
 				<h1>Welcome to ancient one! Fellas.</h1>
 				<p>This is going to be much fun later on. </p>
+				<img src={Sammy} alt='Sammy image' width={200} height={200} />
 			</div>
 		</>
 	)
